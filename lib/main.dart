55e28c1549667e8be6a4ad7bbd4851c0bdb01c0f
zeroupper <mikloshavlik@gmail.com>
2023-11-15 14:44:08 +0100

    fix: add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:magveto/base/routes/routes.dart';
 import 'package:magveto/base/theme/magveto_theme.dart';
+import 'package:magveto/firebase_options.dart';
 
-void main() {
+void main() async {
   final router = GoRouter(routes: $appRoutes);
 
   final theme = MagvetoTheme();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     ProviderScope(
